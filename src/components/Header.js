3748278080804567a14fba391a0dd8c320ecc3a4
sakshik0 <sakshi2k02k@gmail.com>
2024-05-13 19:31:26 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <img alt="logo" src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"></img>
+      <img
+        alt="logo"
+        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+      ></img>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
